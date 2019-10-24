c5a5d6b84d1004999907319fee7f25278d1e0250
Nicolas Raymundo <nicoraymundo2@gmail.com>
2019-10-24 15:19:20 -0700

    Added tab menu and stying for it

--- src/TeamInfo.js
+++ src/TeamInfo.js
@@ -1,4 +1,10 @@
 import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+// import Tabs from "react-bootstrap/Tabs";
+// import Tab from "react-bootstrap/Tab";
+
+// import "react-tabs/style/react-tabs.scss";
+import "./react-tabs.css";
 
 import "./TeamInfo.css";
 
@@ -6,17 +12,37 @@ const TeamInfo = props => {
   const { data } = props;
   // console.log(data);
   return (
-    <div className="TeamInfo">
-      <div className="Circle">
-        <img src={data.logo} alt="blah" id="TeamLogo" />
+    <div className="Teams">
+      <div className="TeamInfo">
+        <div className="Circle">
+          <img src={data.logo} alt="blah" id="TeamLogo" />
+        </div>
+        <div className="TeamData">
+          <p id="TeamName">{data.name}</p>
+          <p>
+            {data.venue_city}, {data.country}
+          </p>
+          <p>{data.venue_name}</p>
+        </div>
       </div>
-      <div className="TeamData">
-        <p id="TeamName">{data.name}</p>
-        <p>
-          {data.venue_city}, {data.country}
-        </p>
-        <p>{data.venue_name}</p>
-      </div>
+
+      <Tabs>
+        <TabList>
+          <Tab>Fixtures</Tab>
+          <Tab>Results</Tab>
+          <Tab>Stats</Tab>
+        </TabList>
+
+        <TabPanel>
+          <p>{data.name}</p>
+        </TabPanel>
+        <TabPanel>
+          <p>{data.venue_city}</p>
+        </TabPanel>
+        <TabPanel>
+          <p>{data.country}</p>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
