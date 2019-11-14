1675e30eda2fdbc1a93a92693d32d444562e6176
Nicolas Raymundo <nicoraymundo2@gmail.com>
2019-11-13 19:35:26 -0800

    Added subtitle

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -6,6 +6,10 @@ import "./AppHeader.css";
 const AppHeader = () => (
   <div className="AppHeader">
     <h1>Take That For Data</h1>
+    <h2>
+      Data from the teams in the Premier League, Bundesliga, Serie A, and La
+      Liga
+    </h2>
   </div>
 );
 
