3b8d949861939f85e0a80f682ce4f5919e273298
Nicolas Raymundo <nicoraymundo2@gmail.com>
2019-11-14 22:28:20 -0800

    Edited styling

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import "./AppHeader.css";
 
-// Note how functional components that don’t have additional logic simply return their markup.
 const AppHeader = () => (
   <div className="AppHeader">
     <h1>Take That For Data</h1>
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import "./SearchResults.css";
 
-// import searchTeams from "./api.js";
-
 import TeamData from "./TeamData";
 import LeagueData from "./LeagueData";
 
@@ -26,19 +24,5 @@ const SearchResults = props => (
         ))}
   </div>
 );
-
-// const SearchResults = props => {
-//   return (
-//     <div className="SearchResults">
-//       {/* {props.results.map(data => (
-//         <LeagueInfo key={data.team_id} data={data} />
-//       ))} */}
-
-//       {props.results.map(data => (
-//         <TeamInfo key={data.team_id} data={data} />
-//       ))}
-//     </div>
-//   );
-// };
 
 export default SearchResults;
