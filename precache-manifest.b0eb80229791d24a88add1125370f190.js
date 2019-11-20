85902149ff81a656a004cb95535062834c4c7e74
Nicolas Raymundo <nicoraymundo2@gmail.com>
2019-11-20 10:44:57 -0800

    Updates

---
+++ precache-manifest.b0eb80229791d24a88add1125370f190.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f25e1bef2efdf1d6605fb96f148eee",
+    "url": "/cmsi370/index.html"
+  },
+  {
+    "revision": "b7255c4106b63b47f679",
+    "url": "/cmsi370/static/css/main.5ab3cdb8.chunk.css"
+  },
+  {
+    "revision": "f860abd714d4010375b5",
+    "url": "/cmsi370/static/js/2.15255470.chunk.js"
+  },
+  {
+    "revision": "b7255c4106b63b47f679",
+    "url": "/cmsi370/static/js/main.f2770f4d.chunk.js"
+  },
+  {
+    "revision": "3ee6572057a398daf448",
+    "url": "/cmsi370/static/js/runtime-main.bffdd8c5.js"
+  }
+]);
